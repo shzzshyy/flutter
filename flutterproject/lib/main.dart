17aef3e2bbbda5419a71c0b342026f359a073ce1
Slark <zyjohnnyb@163.com>
2020-11-18 14:13:03 +0800

    初始化项目

--- flutterproject/lib/main.dart
+++ flutterproject/lib/main.dart
@@ -1,54 +1,64 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-//1.创建main函数
-//2.运行runapp函数  (系统函数)
-//3.传入一个widget
-main() => runApp(MyApp());
-
-//void main() {
-//  //ltr  left  ot right
-//  runApp(MyApp());
-//}
+main () {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Hello world",
-        home: Scaffold(//脚手架
-          appBar: AppBar(
-            title: Text("我是标题"),
-          ),
-          body: ContentWidget(),
-        )
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("暴走的小黑黑"),
+        ),
+        body: HomeContent(),
+      ),
     );
   }
 }
 
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: [
+          ProductItem("APPLE","MacBook","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605688229690&di=bf034dc7263bc26acb455c0686cd5544&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F30%2F29%2F01300000201438121627296084016.jpg"),
+          ProductItem("DUCE","MacAir","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605688229690&di=bf034dc7263bc26acb455c0686cd5544&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F30%2F29%2F01300000201438121627296084016.jpg"),
+          ProductItem("PROMAX","MacMini","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605688229690&di=bf034dc7263bc26acb455c0686cd5544&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F30%2F29%2F01300000201438121627296084016.jpg"),
+        ],
+      ),
+    );
+  }
+}
 
-class ContentWidget extends StatelessWidget {
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imageURL;
+
+  ProductItem(this.title,this.subtitle,this.imageURL);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: TextWidget(),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 3
+        )
+      ),
+      child: Column(
+        children: [
+          Text(title,style: TextStyle(fontSize: 20),),
+          Text(subtitle,style: TextStyle(fontSize: 20),),
+          SizedBox(height: 10,),
+          Image.network(imageURL)
+        ],
       ),
     );
   }
 }
 
-class TextWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "hello world",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-          color: Colors.red,
-          fontSize: 30
-      ),
-    );
-  }
-}
+
